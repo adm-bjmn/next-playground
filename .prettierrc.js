64f780c877174422f7063e8a3d14252a60330cc1
Adam Benjamin <adm.bjmn@gmail.com>
2024-07-22 09:34:44 +0100

    chore: set up eslint rules and prettier settings

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
---
+++ components/NewsDefault.tsx
@@ -0,0 +1,3 @@
+export default function NewDefault() {
+  return <div>Hello</div>;
+}
